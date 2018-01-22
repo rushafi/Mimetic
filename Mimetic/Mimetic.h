95d78bc63a69e8cf878526377d4b444f226ef4de
Wasik Mursalin <wasik@meghdut.io>
2018-01-22 16:53:14 +0600

    Initial Commit

---
+++ Mimetic/Mimetic.h
@@ -0,0 +1,13 @@
+//
+//  Mimetic.h
+//  Mimetic
+//
+//  Created by Wasik Mursalin on 1/22/18.
+//  Copyright © 2018 mblsft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Mimetic : NSObject
+
+@end
---
+++ Mimetic/Mimetic.m
@@ -0,0 +1,13 @@
+//
+//  Mimetic.m
+//  Mimetic
+//
+//  Created by Wasik Mursalin on 1/22/18.
+//  Copyright © 2018 mblsft. All rights reserved.
+//
+
+#import "Mimetic.h"
+
+@implementation Mimetic
+
+@end
