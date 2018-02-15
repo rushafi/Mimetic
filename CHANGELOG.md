5aab358fcb5f9ef85b839175df4ee51b12097e8d
Wasik Mursalin <wasik@meghdut.io>
2018-02-15 13:28:03 +0600

    Add MimeticFramework scheme to enable building as a dynamic framework. Bump version to 0.0.4.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,9 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [0.0.4] - 2018-02-15
+- Add shared framework scheme
+
 ## [0.0.3] - 2018-02-14
 - Reaplce all NSASCIISTRINGENCODING with NSUTF8STRINGENCODING
 - Replace c-style casts with static_cast
---
+++ MimeticFramework/MimeticFramework.h
@@ -0,0 +1,19 @@
+//
+//  MimeticFramework.h
+//  MimeticFramework
+//
+//  Created by Wasik Mursalin on 2/15/18.
+//  Copyright © 2018 mblsft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MimeticFramework.
+FOUNDATION_EXPORT double MimeticFrameworkVersionNumber;
+
+//! Project version string for MimeticFramework.
+FOUNDATION_EXPORT const unsigned char MimeticFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MimeticFramework/PublicHeader.h>
+
+
